42d8adc70130f80cb3c58046fb5de11f2f815bc6
Wesley Gabriel <wesgabriel79@gmail.com>
2024-01-17 00:14:10 -0300

    Adicionando arquivo 'soma.js'

--- src/soma.js
+++ src/soma.js
@@ -9,15 +9,4 @@ function soma(n1,n2){
     return n1+n2;
 };
 
-/*
-Requisitos:
-    - Dentro da pasta src deve existir um arquivo chamado soma.js
-    - Dentro do arquivo soma.js deve existir uma função chamada soma
-    - A função deve ter dois parâmetros numéricos
-    - A função deve retornar o resultado da soma entre os dois parâmetros
-    - A função deve garantir que os parâmetros serão somados e não concatenados
-    - A mensagem de erro "Parâmetros inválidos!" deve ser retornada quando
-    - A função receber apenas o primeiro parâmetro:
-        O primeiro parâmetro não puder ser convertido em um número inteiro
-        O segundo parâmetro não puder ser convertido em um número inteiro
-*/
+console.log(soma(5));

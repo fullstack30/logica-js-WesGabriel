3582e4662ae72898414fab16927e90b6a045f471
WesGabriel <wesgabriel79@gmail.com>
2024-01-19 23:57:13 -0300

    Adicionando arquivo soma_impar.js no repositório.

---
+++ src/soma_impar.js
@@ -0,0 +1,23 @@
+/*
+Requisitos
+    - Dentro da pasta src deve existir um arquivo chamado soma_impar.js
+    - Dentro do arquivo soma_impar.js deve existir uma função chamada soma_impar
+    - A função deve receber um número inteiro positivo
+    - A função deve retornar a soma de todos os números ímpares de 1 até o número passado como argumento
+    - A mensagem de erro "Função aceita apenas números inteiros positivos." deve ser retornada quando
+        O argumento não for do tipo inteiro
+*/
+function soma_impar(numero){
+    if(typeof numero !== "number" || numero<=0 || numero % 1 !== 0){
+        return 'Função aceita apenas números inteiros positivos.'
+    }
+
+    let soma=0;
+
+    for (let i = 1; i <= numero; i+=2) {
+        soma+=i;
+    }
+    return soma;
+}
+
+console.log(soma_impar(65));

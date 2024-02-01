c3a48743bdcd033ff813b501b16ddbf2ced8a045
WesGabriel <wesgabriel79@gmail.com>
2024-02-01 00:08:15 -0300

    Adicionando arquivo do desafio

---
+++ src/fatorial.js
@@ -0,0 +1,28 @@
+/*
+Requisitos: 
+  1 - Dentro da pasta src deve existir um arquivo chamado fatorial.js
+  2 - Dentro do arquivo fatorial.js deve existir uma função chamada fatorial
+  3 - A função deve aceitar como argumento um número inteiro positivo
+  4 - A função deve retornar o resultado do cálculo fatorial
+  5 - A mensagem de erro "Função deve receber um argumento do tipo inteiro positivo." deve ser retornada quando
+    - A função receber um valor numérico não inteiro
+    - A função receber um valor numérico menor que 0
+    - A função receber um valor não numérico
+*/
+function fatorial(n) {
+  if (Number.isInteger(n) == false) {
+    return "Função deve receber um argumento do tipo inteiro positivo.";
+  }
+
+  if (n <= 0) {
+    return "Função deve receber um argumento do tipo inteiro positivo.";
+  }
+
+  let numero = 1;
+  for (let number = 2; number <= n; number++) {
+    numero *= number;
+  }
+  return numero;
+}
+
+console.log(fatorial());

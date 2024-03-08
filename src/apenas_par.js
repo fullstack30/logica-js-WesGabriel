ee541946e727e9c5a150901ddf13575b5ffdd7e2
WesGabriel <wesgabriel79@gmail.com>
2024-03-08 00:09:31 -0300

    Enviando arquivo do desafio apenas_par

---
+++ src/apenas_par.js
@@ -0,0 +1,35 @@
+/*
+Requisitos:
+    1. Dentro da pasta src deve existir um arquivo chamado apenas_par.js
+    2. Dentro do arquivo apenas_par.js deve existir uma função chamada apenas_par
+    3. A função deve receber como argumento um valor do tipo inteiro que pode ser positivo ou negativo
+    4. A função deve retornar uma lista com os números pares entre 0 e o número informado
+    5. A mensagem de erro "Função aceita apenas números inteiros." deve ser retornada quando
+        O argumento passado não for do tipo inteiro
+        Nenhum argumento for passado 
+*/
+
+apenas_par = (n) => {
+  if (typeof n === "number" && !Number.isInteger(n)) {
+    return "Função aceita apenas números inteiros.";
+  }
+
+  let nEven = [];
+  let metrica =
+    n >= 0
+      ? Math.min(0, n % 2 === 0 ? n : n - 1)
+      : Math.max(0, n % 2 === 0 ? n : n + 1);
+  for (let nPar = metrica; nPar <= n; nPar += 2) {
+    if (nPar % 2 === 0) {
+      nEven.push(nPar);
+    }
+  }
+  return nEven;
+};
+
+console.log(apenas_par(5.3) + " teste 1");
+console.log(apenas_par() + " teste 2");
+console.log(apenas_par("teste") + " teste 3");
+console.log(apenas_par(-5.3) + " teste 4");
+console.log(apenas_par(-50) + " teste 5");
+console.log(apenas_par(50) + " teste 6");
